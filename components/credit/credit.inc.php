4ca5fb0c8f0254914d83d73ac9c4e080114c7843
Sherwynd Liew <sherwynd123@gmail.com>
2023-04-11 13:36:57 +0800

    New create for Retirement and Credit

--- components/credit/credit.inc.php
+++ components/credit/credit.inc.php
@@ -1,1 +1,20 @@
-<h1> Code here</h1>
+<!-- credit.inc.php -->
+<div class="flex-grow-1">
+    <!-- ======= Hero Section ======= -->
+    <section id="hero">
+      <div id="heroCarousel" data-bs-interval="5000" class="carousel slide carousel-fade" data-bs-ride="carousel">
+
+        <ol class="carousel-indicators" id="hero-carousel-indicators"></ol>
+
+        <div class="carousel-inner" role="listbox">
+
+        <!-- Slide 5 -->
+        <div
+            class="carousel-item active"
+            style="background-image: url(img/bg-creditscore.jpg); background-position: center">   
+            <div class= "carousel-container">
+                <h2 id="heroTitle" class="animate__animated animate__fadeInDown">Fi<span id="heroTitle-2">NANCE</span></h2>                <div class= "container">
+
+                </div>
+            </div>
+        </div>
