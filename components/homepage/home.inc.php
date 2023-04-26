7232fa04e21886aa0d1ed940bb4f9a0165977e7b
KAE REN <hkr0528@gmail.com>
2023-04-26 22:36:18 +0800

    🏠 Complete landing page design

--- components/homepage/home.inc.php
+++ components/homepage/home.inc.php
@@ -1,31 +1,161 @@
 <!-- home.inc.php -->
 <div class="flex-grow-1">
-    <!-- ======= Hero Section ======= -->
-    <section id="hero">
-      <div id="heroCarousel" data-bs-interval="5000" class="carousel slide carousel-fade" data-bs-ride="carousel">
+  <!-- ======= Hero Section ======= -->
+  <section id="hero">
+    <div id="heroCarousel" data-bs-interval="5000" class="carousel slide carousel-fade" data-bs-ride="carousel">
 
-        <ol class="carousel-indicators" id="hero-carousel-indicators"></ol>
+      <ol class="carousel-indicators" id="hero-carousel-indicators"></ol>
 
-        <div class="carousel-inner" role="listbox">
+      <div class="carousel-inner" role="listbox">
 
-          <!-- Slide 1 -->
-          <div 
-            class="carousel-item active" 
-            style="background-image: url(img/bg-home.png); background-position: center">
-            <div class="carousel-container">
-              <div class="container">
-                <h2 id="heroTitle" class="animate__animated animate__fadeInDown">Fi<span id="heroTitle-2">NANCE</span></h2>
-                <div id="hero-subtitle" class="animate__animated animate__fadeInUp">
-                    <p>where 80 % in 20 %</p>
-                </div>
-                <!-- <a href="#about" class="btn-get-started animate__animated animate__fadeInUp scrollto">Read More</a> -->
+        <!-- Slide 1 -->
+        <div class="carousel-item active" style="background-image: url(img/bg-home.png); background-position: center">
+          <div class="carousel-container">
+            <div class="container">
+              <h2 id="heroTitle" class="animate__animated animate__fadeInDown">Fi<span id="heroTitle-2">NANCE</span></h2>
+              <div id="hero-subtitle" class="animate__animated animate__fadeInUp">
+                <p>where 80 % in 20 %</p>
               </div>
+              <!-- <a href="#about" class="btn-get-started animate__animated animate__fadeInUp scrollto">Read More</a> -->
             </div>
           </div>
+        </div>
 
+      </div>
+    </div>
+  </section>
+  <!-- End Hero -->
+
+  <section id="card-issues" class="mt-3">
+    <p id="cardTitle" class="text-center" style="color:#FFCB33; font-family:'Jost', sans-serif; font-weight: 500; font-size: 48px;">Related Issues</p>
+    <p id="card-subtitle" class="text-center" style="color:#FFCB33; font-family:'Jost', sans-serif; font-weight: 300; font-size: 20px">where all the issues get related</p>
+    <div class="card-deck">
+      <div class="card mx-auto shadow p-3 mb-5 bg-white rounded" style="width: 80%;">
+        <div class="row no-gutters">
+          <div class="col-md-4 order-md-1">
+            <img src="img/bg-home.png" class="card-img" alt="...">
+          </div>
+          <div class="col-md-8 order-md-2 d-flex align-items-center">
+            <div class="card-body">
+              <h5 class="card-title">Issue 1</h5>
+              <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</p>
+              <a href="#" class="btn btn-warning">Read More</a>
+            </div>
+          </div>
         </div>
       </div>
-    </section>
-    <!-- End Hero -->
 
-  </div>
+      <div class="card mx-auto shadow p-3 mb-5 bg-white rounded" style="width: 80%;">
+        <div class="row no-gutters">
+          <div class="col-md-4 order-md-2">
+            <img src="img/bg-home.png" class="card-img" alt="...">
+          </div>
+          <div class="col-md-8 order-md-1 d-flex align-items-center">
+            <div class="card-body">
+              <h5 class="card-title">Issue 1</h5>
+              <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</p>
+              <a href="#" class="btn btn-warning">Read More</a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="card mx-auto shadow p-3 mb-5 bg-white rounded" style="width: 80%;">
+        <div class="row no-gutters">
+          <div class="col-md-4 order-md-1">
+            <img src="img/bg-home.png" class="card-img" alt="...">
+          </div>
+          <div class="col-md-8 order-md-2 d-flex align-items-center">
+            <div class="card-body">
+              <h5 class="card-title">Issue 1</h5>
+              <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</p>
+              <a href="#" class="btn btn-warning">Read More</a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="card mx-auto shadow p-3 mb-5 bg-white rounded" style="width: 80%;">
+        <div class="row no-gutters">
+          <div class="col-md-4 order-md-2">
+            <img src="img/bg-home.png" class="card-img" alt="...">
+          </div>
+          <div class="col-md-8 order-md-1 d-flex align-items-center">
+            <div class="card-body">
+              <h5 class="card-title">Issue 1</h5>
+              <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</p>
+              <a href="#" class="btn btn-warning">Read More</a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="card mx-auto shadow p-3 mb-5 bg-white rounded" style="width: 80%;">
+        <div class="row no-gutters">
+          <div class="col-md-4 order-md-1">
+            <img src="img/bg-home.png" class="card-img" alt="...">
+          </div>
+          <div class="col-md-8 order-md-2 d-flex align-items-center">
+            <div class="card-body">
+              <h5 class="card-title">Issue 1</h5>
+              <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</p>
+              <a href="#" class="btn btn-warning">Read More</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section id="group-member" class="mt-3 mb-5">
+    <p id="cardTitle" class="text-center" style="color:#FFCB33; font-family:'Jost', sans-serif; font-weight: 500; font-size: 48px;">
+      Group Members
+    </p>
+    <p id="card-subtitle" class="text-center" style="color:#FFCB33; font-family:'Jost', sans-serif; font-weight: 300; font-size: 20px">
+      United we stand, divided we fall
+    </p>
+
+    <div class="card-deck d-flex flex-wrap">
+      <div class="card mx-auto col-md-2 col-sm-6 mb-3 mx-3 shadow">
+        <img src="img/profile.jpg" class="card-img" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">HONG KAE REN</h5>
+          <p class="card-text">T text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        </div>
+        <div class="card-footer bg-warning">
+          <small class="text-muted">Front-end Developer</small>
+        </div>
+      </div>
+      <div class="card mx-auto col-md-2 col-sm-6 mb-3 mx-3 shadow">
+        <img src="img/bg-home.png" class="card-img" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+        </div>
+        <div class="card-footer bg-warning">
+          <small class="text-muted">Front-end Developer</small>
+        </div>
+      </div>
+      <div class="card mx-auto col-md-2 col-sm-6 mb-3 mx-3 shadow">
+        <img src="img/bg-home.png" class="card-img" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+        </div>
+        <div class="card-footer bg-warning">
+          <small class="text-muted">Front-end Developer</small>
+        </div>
+      </div>
+      <div class="card mx-auto col-md-2 col-sm-6 mb-3 mx-3 shadow">
+        <img src="img/bg-home.png" class="card-img" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+        </div>
+        <div class="card-footer bg-warning">
+          <small class="text-muted">Front-end Developer</small>
+        </div>
+      </div>
+    </div>
+  </section>
+</div>
