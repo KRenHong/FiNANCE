fd9ef9d6d32be38bfb62031c2cf89251b3060e11
PeiWei02 <npwlol54@gmail.com>
2023-04-28 02:44:11 +0800

    investment part first commit

--- components/investments/investments.inc.php
+++ components/investments/investments.inc.php
@@ -1,1 +1,85 @@
-<h1> Code here</h1>
+<!-- home.inc.php -->
+<div class="flex-grow-1">
+  <!-- ======= Hero Section ======= -->
+  <section id="hero">
+    <div id="heroCarousel" data-bs-interval="5000" class="carousel slide carousel-fade" data-bs-ride="carousel">
+
+      <ol class="carousel-indicators" id="hero-carousel-indicators"></ol>
+
+      <div class="carousel-inner" role="listbox">
+
+        <!-- Slide 1 -->
+        <div class="carousel-item active" style="background-image: url(img/invesment.png); background-position: center">
+          <div class="carousel-container">
+            <div class="container">
+              <h2 id="heroTitle" class="animate__animated animate__fadeInDown">I<span id="heroTitle-2">NVESTMENT</span>
+              </h2>
+
+              <p class="text-justify">The act of managing funds or resources with the hope of making a profit or
+                producing revenue
+                is known as investing. In order to get a return on your investment, investing requires taking on risk.
+                Stocks,
+                bonds, mutual funds, real estate, and alternative investments are just a few of the many different kinds
+                of
+                investments. Investment involves thorough market research, analysis, and knowledge of all possible
+                investment
+                possibilities.</p>
+
+
+              <!-- <a href="#about" class="btn-get-started animate__animated animate__fadeInUp scrollto">Read More</a> -->
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </section>
+</div>
+
+
+<section class="body">
+  <div class="container">
+    <div class="row py-5">
+      <div class="col-lg-12 text-start">
+        <h1 id="heroTitle">Invest Earlier, Enjoy Earlier</h1>
+        <p class="lead">By starting their investments early in life, investors can benefit from the force
+          of
+          compounding, which enables their money to grow over time. Early, even little investments can have a big
+          long-term impact on an investor's portfolio. Beginning their investments sooner might provide investors more
+          time in the market, which can lessen the effects of market volatility and increase their opportunities for
+          gain. Investors can more quickly and easily attain their financial goals by taking advantage of the
+          advantages
+          of investing sooner. Following are some examples of compound effects:</p>
+      </div>
+      <img src="img/Compounding.png" class="card-img" alt="...">
+    </div>
+    <div class="row py-5">
+      <div class="col-lg-12 text-start">
+        <h1 id="heroTitle">How to Start Investing</h1>
+        <p class="lead">Building long-term wealth and establishing financial security require investing, which is a
+          crucial step. While starting to invest, keep in mind these steps:</p>
+        <ul class="lead text-left">
+          <li>Set your investment goals: Before you begin investing, it's important to have a clear idea of what
+            you're
+            working towards. This might include saving for retirement, a down payment on a house, or funding your
+            child's education. Knowing your goals will help you make informed investment decisions that align with
+            your
+            objectives.</li>
+          <li>Assess your risk tolerance: Making investment selections requires a thorough understanding of your risk
+            tolerance. Based on variables including age, income, and investment objectives, each person's risk
+            tolerance
+            will be unique. The general rule is that younger investors can afford to take on more risk in their
+            portfolios, whereas those who are nearing retirement may wish to concentrate on more cautious investments.
+          </li>
+          <li>Pick your investments: Stocks, bonds, mutual funds, exchange-traded funds (ETFs), and other financial
+            instruments may be included. It's critical to conduct due diligence on each investment choice and
+            comprehend
+            the dangers and potential rewards.</li>
+          <li>Observe and make changes to your portfolio: As investing is a continuous activity, it's critical to keep
+            an eye on your holdings and modify your portfolio as needed to reflect changes in the market and your
+            investment objectives.</li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</section>
