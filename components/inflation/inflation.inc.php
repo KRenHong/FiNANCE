422bc42993544c093d02b1a4e7893d682d112a51
KAE REN <hkr0528@gmail.com>
2023-04-28 23:04:11 +0800

    🎈 Done inflation page

--- components/inflation/inflation.inc.php
+++ components/inflation/inflation.inc.php
@@ -1,1 +1,180 @@
-<h1> Code here</h1>
+<section>
+  <!-- Hero -->
+  <div class="p-5 d-flex text-left bg-image justify-content-start align-items-center" style="
+    background-image: url('img/balloon.gif');
+    height: 650px;
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+  ">
+    <div class="mask rounded-3" style="background-color: rgba(0, 0, 0, 0.6);">
+      <div class="d-flex justify-content-center align-items-center h-100">
+        <div class="text-white">
+          <h1 class="mb-3" style="
+          font-size: 5rem;
+          font-family: 'Jost', sans-serif;
+          font-weight: 700;
+        ">
+            <span style="color:#FFCB33">In</span>flation
+          </h1>
+          <h4 class="mb-3" style="
+          font-family: 'Jost', sans-serif;
+        
+        ">Learn about inflation and how it affects you</h4>
+          <a class="btn btn-outline-warning btn-lg" href="#main-content" role="button">Read More</a>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- Hero -->
+</section>
+
+<section id="main-content">
+  <!-- Main Content -->
+  <div class="container my-5">
+    <h1 id="main-title" class="text-center" style="
+      font-family: 'Jost', sans-serif;
+      font-size: 64px;
+      color: #FFCB33;
+    
+    ">Inflation</h1>
+    <div class="row mt-5">
+      <div class="col-md-6">
+        <h3>What is Inflation?</h3>
+        <p>Inflation is a sustained increase in the general price level of goods and services in an economy over a period of time. This can lead to a decrease in the purchasing power of money and can have negative effects on individuals, businesses, and the economy as a whole.</p>
+      </div>
+      <div class="col-md-6">
+        <img src="https://i.ytimg.com/vi/HQ-Kg_xgdhE/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLD8kRvMCaZHzFww_etOm4CoCa_JlQ" class="img-fluid" alt="Inflation" width="600" height="200">
+      </div>
+    </div>
+    <div class="row mt-5">
+      <div class="col-md-6">
+        <img src="https://i.ytimg.com/vi/pmBihTRFVNM/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBaLudAs-e9A5dWUOf3btpYQjhtYg" class="img-fluid" alt="Inflation" width="600" height="300">
+      </div>
+      <div class="col-md-6">
+        <h3>Causes of Inflation</h3>
+        <div class="accordion" id="accordionExample">
+          <div class="card border-3 border-warning mb-3">
+            <div class="card-header" id="headingOne">
+              <h2 class="mb-0">
+                <button class="btn btn-link text-muted" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne" style="text-decoration: none;">
+                  Demand-Pull Inflation
+                </button>
+              </h2>
+            </div>
+            <div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#accordionExample">
+              <div class="card-body">
+                This type of inflation occurs when there is an increase in demand for goods and services that exceeds the available supply, resulting in an increase in prices.
+              </div>
+            </div>
+          </div>
+          <div class="card border-3 border-warning mb-3">
+            <div class="card-header" id="headingTwo">
+              <h2 class="mb-0">
+                <button class="btn btn-link collapsed text-muted" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo" style="text-decoration: none;">
+                  Cost-Push Inflation
+                </button>
+              </h2>
+            </div>
+            <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
+              <div class="card-body">
+                This type of inflation occurs when the cost of production increases, causing producers to increase the prices of their goods and services to maintain their profit margins. </div>
+            </div>
+          </div>
+          <div class="card border-3 border-warning mb-3">
+            <div class="card-header" id="headingThree">
+              <h2 class="mb-0">
+                <button class="btn btn-link collapsed text-muted" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree" style="text-decoration: none;">
+                  Built-In Inflation
+                </button>
+              </h2>
+            </div>
+            <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
+              <div class="card-body">
+                This type of inflation occurs when workers demand higher wages to keep up with rising prices, and businesses increase prices to cover the higher labor costs, creating a cycle of price increases. </div>
+            </div>
+          </div>
+          <div class="card border-3 border-warning">
+            <div class="card-header" id="headingFour">
+              <h2 class="mb-0">
+                <button class="btn btn-link collapsed text-muted" type="button" data-toggle="collapse" data-target="#collapseFour" aria-expanded="false" aria-controls="collapseThree" style="text-decoration: none;">
+                  Monetary Inflation </button>
+              </h2>
+            </div>
+            <div id="collapseFour" class="collapse" aria-labelledby="headingFour" data-parent="#accordionExample">
+              <div class="card-body">
+                This type of inflation occurs when there is an increase in the money supply, causing the value of money to decrease and prices to rise. </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+</section>
+
+<section class="bg-warning py-5">
+  <div class="container">
+    <h2 class="text-dark mb-4 text-center">Inflation: Effects &amp; Control Measures</h2>
+    <div id="inflationCarousel" class="carousel slide" data-bs-ride="carousel">
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <div class="row">
+            <div class="col-md-6">
+              <div class="card mb-3 border-0">
+                <img src="https://i.ytimg.com/vi/xPe7q-U3XBw/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBhWwVUY37qvICyOwaMlbNSyY1lLQ" class="card-img-top" alt="Effects of Inflation">
+                <div class="card-body">
+                  <h5 class="card-title">Effects of Inflation</h5>
+                  <p class="card-text">Inflation affects various aspects of an economy, including purchasing power, income distribution, savings, and investments.</p>
+                  <a href="https://www.youtube.com/watch?v=xPe7q-U3XBw" class="btn btn-dark">Learn more</a>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-6">
+              <div class="card mb-3 border-0">
+                <img src="https://i.ytimg.com/vi/F54-D_9ci0I/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDj85PLX00Xx5m4I6ziE52EXHVKaw" class="card-img-top" alt="Control Measures">
+                <div class="card-body">
+                  <h5 class="card-title">Measures to Control Inflation</h5>
+                  <p class="card-text">Governments and central banks use various measures to control inflation and stabilize the economy, such as monetary policy and fiscal policy.</p>
+                  <a href="https://www.youtube.com/watch?v=F54-D_9ci0I" class="btn btn-dark">Learn more</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <div class="row">
+            <div class="col-md-6">
+              <div class="card mb-3 border-0">
+                <img src="https://i.ytimg.com/vi/7PKY2_YQFVE/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAD1LlpOAesONn2wwRiptbUx1dKOw" class="card-img-top" alt="Supply-side Policies">
+                <div class="card-body">
+                  <h5 class="card-title">Supply-side Policies</h5>
+                  <p class="card-text">Policies aimed at increasing productivity and the efficient allocation of resources can help control inflation.</p>
+                  <a href="https://www.youtube.com/watch?v=7PKY2_YQFVE" class="btn btn-dark">Learn more</a>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-6">
+              <div class="card mb-3 border-0">
+                <img src="https://i.ytimg.com/vi/0xMCWr0O3Hs/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLC4llPP6aEVCvXA-XwBFJuTQzb5vQ" class="card-img-top" alt="Incomes Policies">
+                <div class="card-body">
+                  <h5 class="card-title">Incomes Policies</h5>
+                  <p class="card-text">Wage and price controls can be implemented to directly limit the growth of prices and wages, thereby controlling inflation.</p>
+                  <a href="https://www.youtube.com/watch?v=0xMCWr0O3Hs" class="btn btn-dark">Learn more</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <button class="carousel-control-prev" type="button" data-bs-target="#inflationCarousel" data-bs-slide="prev" style="left: -150px;">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+      </button>
+      <button class="carousel-control-next" type="button" data-bs-target="#inflationCarousel" data-bs-slide="next" style="right: -150px;">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+      </button>
+    </div>
+
+  </div>
+</section>
--- inflation.php
+++ inflation.php
@@ -1,3 +1,4 @@
 <?php require('components/head.inc.php'); ?>
 <?php include('components/navbar.inc.php'); ?>
 <?php include('components/inflation/inflation.inc.php'); ?>
+<?php include('components/footer.inc.php'); ?>
