a0e0342b916e7349861d027db10f5bb808d88db0
PeiWei02 <npwlol54@gmail.com>
2023-05-01 01:21:44 +0800

    investment part done

--- components/investments/investments.inc.php
+++ components/investments/investments.inc.php
@@ -1,18 +1,49 @@
-<!-- home.inc.php -->
+<section>
+  <!-- Hero -->
+  <div class="p-5 d-flex text-left bg-image justify-content-start align-items-center" style="
+    background-image: url('img/money.gif');
+    height: 650px;
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+  ">
+    <div class="mask rounded-3 py-3 px-3" style="background-color: rgba(0, 0, 0, 0.6);">
+      <div class="d-flex justify-content-center align-items-center h-100">
+        <div class="text-white">
+          <h1 class="mb-3" style="
+          font-size: 5rem;
+          font-family: 'Jost', sans-serif;
+          font-weight: 700;
+        ">
+            <span style="color:#FFCB33">In</span>vestment
+          </h1>
+          <h4 class="mb-3" style="
+          font-family: 'Jost', sans-serif;
+        
+        ">Invest earlier enjoy earlier</h4>
+          <a class="btn btn-outline-warning btn-lg" href="#heroCarousel" role="button">Understand more</a>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- Hero -->
+</section>
+
+
 <div class="flex-grow-1">
-  <!-- ======= Hero Section ======= -->
   <section id="hero">
-    <div id="heroCarousel" data-bs-interval="5000" class="carousel slide carousel-fade" data-bs-ride="carousel">
+    <div id="heroCarousel" data-bs-interval="5000" class="carousel slide carousel-fade py-5" data-bs-ride="carousel">
 
       <ol class="carousel-indicators" id="hero-carousel-indicators"></ol>
 
       <div class="carousel-inner" role="listbox">
 
         <!-- Slide 1 -->
-        <div class="carousel-item active" style="background-image: url(img/invesment.png); background-position: center">
+        <div class="carousel-item active" style="background-image: url(img/.png); background-position: center">
           <div class="carousel-container">
             <div class="container">
-              <h2 id="heroTitle" class="animate__animated animate__fadeInDown">I<span id="heroTitle-2">NVESTMENT</span>
+              <h2 id="heroTitle" class="animate__animated animate__fadeInDown position">What is<span id="heroTitle-2">
+                  INVESTMENT ?</span>
               </h2>
 
               <p class="text-justify">The act of managing funds or resources with the hope of making a profit or
@@ -40,9 +71,10 @@
 <section class="body">
   <div class="container">
     <div class="row py-5">
-      <div class="col-lg-12 text-start">
-        <h1 id="heroTitle">Invest Earlier, Enjoy Earlier</h1>
-        <p class="lead">By starting their investments early in life, investors can benefit from the force
+      <div class="col-lg-6 text-start">
+        <h1 class="pt-3 text-warning">Invest Earlier,
+          Enjoy Earlier!</h1>
+        <p class="lead py-4">By starting their investments early in life, investors can benefit from the force
           of
           compounding, which enables their money to grow over time. Early, even little investments can have a big
           long-term impact on an investor's portfolio. Beginning their investments sooner might provide investors more
@@ -51,35 +83,138 @@
           advantages
           of investing sooner. Following are some examples of compound effects:</p>
       </div>
-      <img src="img/Compounding.png" class="card-img" alt="...">
+      <div class="col-lg-6 text-start py-3">
+        <img src="img/Compounding.webp" class="card-img" alt="...">
+      </div>
     </div>
-    <div class="row py-5">
-      <div class="col-lg-12 text-start">
-        <h1 id="heroTitle">How to Start Investing</h1>
+    <div class="row ">
+      <div class="col-lg-6 text-start ">
+        <img src="img/start-investing.jpg" class="card-img" alt="...">
+      </div>
+      <div class="col-lg-6 text-start pb-3 mb-5">
         <p class="lead">Building long-term wealth and establishing financial security require investing, which is a
           crucial step. While starting to invest, keep in mind these steps:</p>
-        <ul class="lead text-left">
-          <li>Set your investment goals: Before you begin investing, it's important to have a clear idea of what
-            you're
-            working towards. This might include saving for retirement, a down payment on a house, or funding your
-            child's education. Knowing your goals will help you make informed investment decisions that align with
-            your
-            objectives.</li>
-          <li>Assess your risk tolerance: Making investment selections requires a thorough understanding of your risk
-            tolerance. Based on variables including age, income, and investment objectives, each person's risk
-            tolerance
-            will be unique. The general rule is that younger investors can afford to take on more risk in their
-            portfolios, whereas those who are nearing retirement may wish to concentrate on more cautious investments.
-          </li>
-          <li>Pick your investments: Stocks, bonds, mutual funds, exchange-traded funds (ETFs), and other financial
-            instruments may be included. It's critical to conduct due diligence on each investment choice and
-            comprehend
-            the dangers and potential rewards.</li>
-          <li>Observe and make changes to your portfolio: As investing is a continuous activity, it's critical to keep
-            an eye on your holdings and modify your portfolio as needed to reflect changes in the market and your
-            investment objectives.</li>
-        </ul>
+        <div class="accordion" id="accordionExample">
+          <div class="card border-3 border-warning mb-3">
+            <div class="card-header" id="headingOne">
+              <h2 class="mb-0">
+                <button class="btn btn-link text-muted" type="button" data-toggle="collapse" data-target="#collapseOne"
+                  aria-expanded="true" aria-controls="collapseOne" style="text-decoration: none;">
+                  Set your investment goals
+                </button>
+              </h2>
+            </div>
+            <div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#accordionExample">
+              <div class="card-body">
+                Before you begin investing, it's important to have a clear idea of what
+                you're
+                working towards. This might include saving for retirement, a down payment on a house, or funding your
+                child's education. Knowing your goals will help you make informed investment decisions that align with
+                your
+                objectives.
+              </div>
+            </div>
+          </div>
+          <div class="card border-3 border-warning mb-3">
+            <div class="card-header" id="headingTwo">
+              <h2 class="mb-0">
+                <button class="btn btn-link collapsed text-muted" type="button" data-toggle="collapse"
+                  data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo"
+                  style="text-decoration: none;">
+                  Assess your risk tolerance
+                </button>
+              </h2>
+            </div>
+            <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
+              <div class="card-body">
+                Making investment selections requires a thorough understanding of your risk
+                tolerance. Based on variables including age, income, and investment objectives, each person's risk
+                tolerance
+                will be unique. The general rule is that younger investors can afford to take on more risk in their
+                portfolios, whereas those who are nearing retirement may wish to concentrate on more cautious
+                investments. </div>
+            </div>
+          </div>
+          <div class="card border-3 border-warning mb-3">
+            <div class="card-header" id="headingThree">
+              <h2 class="mb-0">
+                <button class="btn btn-link collapsed text-muted" type="button" data-toggle="collapse"
+                  data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree"
+                  style="text-decoration: none;">
+                  Pick your investments
+                </button>
+              </h2>
+            </div>
+            <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
+              <div class="card-body">
+                Stocks, bonds, mutual funds, exchange-traded funds (ETFs), and other financial
+                instruments may be included. It's critical to conduct due diligence on each investment choice and
+                comprehend </div>
+            </div>
+          </div>
+          <div class="card border-3 border-warning">
+            <div class="card-header" id="headingFour">
+              <h2 class="mb-0">
+                <button class="btn btn-link collapsed text-muted" type="button" data-toggle="collapse"
+                  data-target="#collapseFour" aria-expanded="false" aria-controls="collapseThree"
+                  style="text-decoration: none;">
+                  Observe and make changes to your portfolio </button>
+              </h2>
+            </div>
+            <div id="collapseFour" class="collapse" aria-labelledby="headingFour" data-parent="#accordionExample">
+              <div class="card-body">
+                As investing is a continuous activity, it's critical to keep
+                an eye on your holdings and modify your portfolio as needed to reflect changes in the market and your
+                investment objectives. </div>
+            </div>
+          </div>
+        </div>
       </div>
+    </div>
+  </div>
+</section>
+
+<section class="stock_recommend">
+  <div class="mt-4  pt-4 " style="background-color: #252525;">
+    <h1 id="heroTitle" class="mx-auto text-center justify-content-center text-warning text-bold font-weight-bold">
+      Popular Stocks</h1>
+    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+      <ol class="carousel-indicators">
+        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+      </ol>
+      <div class="carousel-inner">
+        <div class="carousel-item active c-item">
+          <img class="d-block w-100" src="img/Stocks_Aapl.png" alt="First slide">
+          <div class="carousel-caption d-none d-md-block">
+            <h5 id="heroTitle" class="display-1 fw-bolder text-capitalize">AAPL </h5>
+            <p id="hero-subtitle" class="text-uppercase fs-5 ">Current Value: 169.68USD</p>
+          </div>
+        </div>
+        <div class="carousel-item c-item">
+          <img class="d-block w-100" src="img/Stocks_Msft.png" alt="Second slide">
+          <div class="carousel-caption d-none d-md-block">
+            <h5 id="heroTitle" class="display-1 fw-bolder text-capitalize">MSFT</h5>
+            <p id="hero-subtitle" class="text-uppercase fs-5 ">Current Value: 307.26USD</p>
+          </div>
+        </div>
+        <div class="carousel-item c-item">
+          <img class="d-block w-100" src="img/stocks-tsla.png" alt="Third slide">
+          <div class="carousel-caption d-none d-md-block">
+            <h5 id="heroTitle" class="display-1 fw-bolder text-capitalize">TSLA</h5>
+            <p id="hero-subtitle" class="text-uppercase fs-5">Current Value: 164.31USD</p>
+          </div>
+        </div>
+      </div>
+      <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
     </div>
   </div>
 </section>
