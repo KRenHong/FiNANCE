fce9b4c9819da076cbd2de618343b5f95cd4ff80
Saber97K <114329656+Saber97K@users.noreply.github.com>
2023-05-01 20:07:45 +0800

    Commit2

--- debt.php
+++ debt.php
@@ -1,3 +1,3 @@
-<?php require('components/head.inc.php'); ?>
+<?php require('components/debt/head.inc.php'); ?>
 <?php include('components/navbar.inc.php'); ?>
 <?php include('components/debt/debt.inc.php'); ?>
