729ebc321effe325914855f746bb1cc99e8ab151
Sherwynd Liew <sherwynd123@gmail.com>
2023-05-02 00:05:14 +0800

    UI done

--- components/retirement/retirement.inc.php
+++ components/retirement/retirement.inc.php
@@ -23,18 +23,40 @@
 </section>
         <!-- Slide 2 -->
     <section class="bodyR">
-        <div class="container">
-            <div id="layout" class="stars">
-                <span class="star"></span>
-                <span class="star pink"></span>
-                <span class="star blue"></span>
-                <span class="star yellow"></span>
-            </div>
-        </div>
-            <div class="content">
-                <h2 id="contentTitle"></h2>
-
-                <p class="cont"></p>
-            </div>
+        <div class="smalltain">
+            <!-- <video autoplay loop muted plays-inline class="background-clip">
+                <source src="" type="img/work.mp4"></source>
+            </video> -->
+                <ul class="accordion">
+                    
+                    <a href="#" style="--clr:#3e9aae"><span>Understanding</span><i></i></a>
+                        <li>
+                            <input type="radio" name="accordion" id="first" checked>
+                            <label for="first">Understanding Retirement Planning</label>
+                            <div class="content">
+                                <p>In the simplest sense, retirement planning is what one does to be prepared for life after paid work ends. This isn't just financially but in all aspects of life.<br><br>The non-financial aspects include lifestyle choices such as how to spend time in retirement, where to live, and when to quit working altogether, among other things. A holistic approach to retirement planning considers all these areas.</p>
+                            </div>
+                            <img src="img/retireplan.gif" class="imageretire1">
+                        </li>
+                        <a href="#" style="--clr:#3e9aae"><span>Retirement</span><i></i></a>
+                        <li>
+                            <input type="radio" name="accordion" id="second">
+                            <label for="second">How Much Do You Need to Retire?</label>
+                            <div class="content">
+                                <p>Remember that retirement planning starts long before you retire. The general rule is the sooner you start, the better. Your magic number, which is the amount you need to retire comfortably, is highly personalized. But there are numerous rules of thumb that can give you an idea of how much to save.<br><br>
+                                While the amount of money you'll want to have in your nest egg is important, it's also a good idea to consider all of your expenses. Be sure to calculate the costs for housing, health insurance, food, clothing, and your vehicle/transportation. And since you'll have more free time on your hands, you may also want to factor in the cost of entertainment and travel. While it may be hard to come up with concrete figures, be sure to come up with a reasonable estimate so there are no surprises later on.</p>
+                            </div>
+                            <img src="img/plantoretire.gif" class="imageretire1">
+                        </li>
+                        <a href="#" style="--clr:#3e9aae"><span>Planning</span><i></i></a>
+                        <li>
+                            <input type="radio" name="accordion" id="third">
+                            <label for="third">Why Is Retirement Planning So Important?</label>
+                            <div class="content">
+                                <p>Retirement planning allows you to sock away enough money to maintain the same lifestyle you currently have. After all, no one wants to work right up until the end. While you may work part-time or pick up the odd gig here or there, it probably won't be enough to sustain your current lifestyle. And Social Security benefits will only take you so far. That's why it's so important to have a viable plan that allows you to get the maximum amount of money when you retire.</p>
+                            </div>
+                            <img src="img/plan.gif" class="imageretire1">
+                        </li>
+                </ul>   
         </div>
     </section>
