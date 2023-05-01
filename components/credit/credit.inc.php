e4c7671e44b9475bb5c2f8a3aa4aafdd21d3f76c
Sherwynd Liew <sherwynd123@gmail.com>
2023-05-02 03:55:02 +0800

    c

--- components/credit/credit.inc.php
+++ components/credit/credit.inc.php
@@ -13,7 +13,7 @@
         <!-- Slide 1 -->
         <div
             class="carousel-item active"
-            style="background-image: url(img/bg-creditscore.jpg); background-position: center">   
+            style="background-image: url(img/score.gif); background-position: center">
             <div class= "carousel-container">
             <h2 id="heroTitle" class="animate__animated animate__fadeInDown">C<span id="heroTitle-2">redit </span>S<span id="heroTitle-2">core</span></h2>            
             <div class= "container">
@@ -55,3 +55,71 @@
         <img src="img/vantage.png" class="imgMain">
 
     </section>
+
+    <!-- Hyperlink Video -->
+    <section class="bg-warning py-5">
+  <div class="container">
+    <h2 class="text-dark mb-4 text-center">Inflation: Effects &amp; Control Measures</h2>
+    <div id="inflationCarousel" class="carousel slide" data-bs-ride="carousel">
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <div class="row">
+            <div class="col-md-6">
+              <div class="card mb-3 border-0">
+                <img src="https://i.ytimg.com/vi/xPe7q-U3XBw/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBhWwVUY37qvICyOwaMlbNSyY1lLQ" class="card-img-top" alt="Effects of Inflation">
+                <div class="card-body">
+                  <h5 class="card-title">Effects of Inflation</h5>
+                  <p class="card-text">Inflation affects various aspects of an economy, including purchasing power, income distribution, savings, and investments.</p>
+                  <a href="https://www.youtube.com/watch?v=xPe7q-U3XBw" class="btn btn-dark">Learn more</a>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-6">
+              <div class="card mb-3 border-0">
+                <img src="https://i.ytimg.com/vi/F54-D_9ci0I/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDj85PLX00Xx5m4I6ziE52EXHVKaw" class="card-img-top" alt="Control Measures">
+                <div class="card-body">
+                  <h5 class="card-title">Measures to Control Inflation</h5>
+                  <p class="card-text">Governments and central banks use various measures to control inflation and stabilize the economy, such as monetary policy and fiscal policy.</p>
+                  <a href="https://www.youtube.com/watch?v=F54-D_9ci0I" class="btn btn-dark">Learn more</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <div class="row">
+            <div class="col-md-6">
+              <div class="card mb-3 border-0">
+                <img src="https://i.ytimg.com/vi/7PKY2_YQFVE/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAD1LlpOAesONn2wwRiptbUx1dKOw" class="card-img-top" alt="Supply-side Policies">
+                <div class="card-body">
+                  <h5 class="card-title">Supply-side Policies</h5>
+                  <p class="card-text">Policies aimed at increasing productivity and the efficient allocation of resources can help control inflation.</p>
+                  <a href="https://www.youtube.com/watch?v=7PKY2_YQFVE" class="btn btn-dark">Learn more</a>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-6">
+              <div class="card mb-3 border-0">
+                <img src="https://i.ytimg.com/vi/0xMCWr0O3Hs/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLC4llPP6aEVCvXA-XwBFJuTQzb5vQ" class="card-img-top" alt="Incomes Policies">
+                <div class="card-body">
+                  <h5 class="card-title">Incomes Policies</h5>
+                  <p class="card-text">Wage and price controls can be implemented to directly limit the growth of prices and wages, thereby controlling inflation.</p>
+                  <a href="https://www.youtube.com/watch?v=0xMCWr0O3Hs" class="btn btn-dark">Learn more</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <button class="carousel-control-prev" type="button" data-bs-target="#inflationCarousel" data-bs-slide="prev" style="left: -150px;">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+      </button>
+      <button class="carousel-control-next" type="button" data-bs-target="#inflationCarousel" data-bs-slide="next" style="right: -150px;">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+      </button>
+    </div>
+
+  </div>
+</section>
