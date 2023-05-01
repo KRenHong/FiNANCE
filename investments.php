fc31e189b1da047c4ddeb8768497e20c616e21ba
PeiWei02 <npwlol54@gmail.com>
2023-05-01 18:16:20 +0800

    Update investments.php

--- investments.php
+++ investments.php
@@ -1,3 +1,4 @@
 <?php require('components/head.inc.php'); ?>
 <?php include('components/navbar.inc.php'); ?>
 <?php include('components/investments/investments.inc.php'); ?>
+<?php include('components/footer.inc.php'); ?>
